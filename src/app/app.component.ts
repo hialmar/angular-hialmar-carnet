ce1a01247d38314a0649d9e49636a985208a44c5
Patrice Torguet <torguet@gmail.com>
2020-01-08 15:12:08 +0100

    Sources sauvegardées suite à un bug stackblitz.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
+import { CarnetService } from './carnet.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+
+  constructor(public carnet: CarnetService,
+  private router: Router) {
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CarnetService } from './carnet.service';
+import { AjoutComponent } from './ajout/ajout.component';
+import { ListComponent } from './list/list.component';
+
+const appRoutes: Routes = [
+  {path: 'list', component: ListComponent},
+  {path: 'list/add', component: AjoutComponent},
+  {path: 'list/view/:id', component: AjoutComponent},
+  {path: '', redirectTo: 'list', pathMatch: 'full'},
+  {path: '**', redirectTo: 'list'}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, HelloComponent, AjoutComponent, ListComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [CarnetService,
+    {provide: APP_BASE_HREF, useValue: '/'}]
 })
 export class AppModule { }
